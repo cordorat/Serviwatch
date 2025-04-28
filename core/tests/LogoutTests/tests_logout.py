aba8f4236dc9d0c0eea006ce2c0bf550a8d49cf7
Anthony Alzate <anthony.alzate@correounivalle.edu.co>
2025-04-28 16:49:04 -0500

    Implementación Login

--- core/tests/LogoutTests/tests_logout.py
+++ core/tests/LogoutTests/tests_logout.py
@@ -6,12 +6,23 @@ from django.http import HttpRequest
 from django.contrib.sessions.middleware import SessionMiddleware
 from unittest.mock import patch, MagicMock
 from core.services.logout_service import cerrar_sesion
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 class LogoutViewTest(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='12345'
+        )
+        
     @patch('core.views.Logout.logout_view.cerrar_sesion')
     def test_logout_view_calls_cerrar_sesion(self, mock_cerrar_sesion):
         # Arrange
         request = HttpRequest()
+        request.user = self.user
+        request.method = 'POST'  # Because we have @require_http_methods(["POST"])
         
         # Act
         response = logout_view(request)
@@ -21,12 +32,14 @@ class LogoutViewTest(TestCase):
         self.assertEqual(response.url, reverse('login'))
 
     def test_logout_view_redirects_to_login(self):
+        # Arrange
+        self.client.force_login(self.user)
+        
         # Act
-        response = self.client.get('/logout/')
+        response = self.client.post('/logout/')  # Changed to POST request
         
         # Assert
         self.assertRedirects(response, reverse('login'), fetch_redirect_response=False)
-
 
 class LogoutServiceTest(TestCase):
     def setUp(self):
