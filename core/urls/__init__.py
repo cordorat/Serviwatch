e6240a37909b7c5f0deb9c3d77561eab1744977d
lucassalna <lucassalna06@gmail.com>
2025-04-28 12:03:14 -0500

    Implementacion de login con css (bien)
    * implementacion del login
    
    * modificacion del login
    
    * migraciones de base de datos
    
    * revision
    
    * arreglo de test
    
    ---------
    
    Co-authored-by: lucas <lucassalna06gmail.com>

--- core/urls/__init__.py
+++ core/urls/__init__.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('core.urls.cliente_urls')),
+    path('', include('core.urls.login_urls')),
     path('', include('core.urls.reparacion_urls')),
     path('', include('core.urls.empleado_urls')),
 ]
