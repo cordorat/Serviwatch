68b1777775027a108a86f71cf54331d8f3d2ddc0
luna171717 <lunacristhian866@gmail.com>
2025-04-29 21:50:31 -0500

    implementación de la hu recuperar contraseña
    * todo funcional
    
    * hu recuperar contraseña terminada.

--- core/urls/login_urls.py
+++ core/urls/login_urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from core.views.login.login_view import login_view
+from core.views.login.recuperar_contraseña import recuperar_contraseña, cambiar_contraseña
 
 urlpatterns = [
     path('', login_view, name='login'),
+    path('recuperarContraseña/', recuperar_contraseña, name='recuperar_contraseña'),
+    path('cambiarContraseña/<str:token>/', cambiar_contraseña, name='cambiar_contraseña'),
 ]
+
