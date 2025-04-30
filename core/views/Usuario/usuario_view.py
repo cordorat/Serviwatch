0f6ace296b3c4bd6a9759c4a064f30c95a54f46a
Sara Sinisterra Villota <sinisterra.sara@correounivalle.edu.co>
2025-04-30 00:11:10 -0500

    agregar usuario completado con mensajes resaltados en rojo

--- core/views/Usuario/usuario_view.py
+++ core/views/Usuario/usuario_view.py
@@ -4,25 +4,34 @@ from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from core.forms.usuario_form import FormularioRegistroUsuario
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 
+@login_required
+@require_http_methods(["GET"])
 def usuario_list_view(request):
-    return render(request, "usuario/usuario_list.html")
+    usuarios = User.objects.all()
+    return render(request, "usuario/usuario_list.html",{"usuarios":usuarios})
 
-
+@login_required
+@require_http_methods(["GET", "POST"])
 def usuario_create_view(request):
-    if request.method == 'GET':
-        return render(request, 'usuario/usuario_form.html', {
-            'form': FormularioRegistroUsuario()
-        })
-    else:
+    if request.method == 'POST':
         form = FormularioRegistroUsuario(request.POST)
         if form.is_valid():
             try:
                 user = form.save()
-                login(request, user)
-                return redirect('usuario_create')
+                messages.success(request, 'Usuario creado exitosamente')
+                return redirect('usuario_list')  
             except IntegrityError:
                 form.add_error('username', 'El nombre de usuario ya existe')
+                messages.error(request, 'Error al crear el usuario')
+        else:
+            messages.error(request, 'Por favor corrija los errores en el formulario')
+            print(form.errors) 
+    else:
+        form = FormularioRegistroUsuario()
+    
     return render(request, 'usuario/usuario_form.html', {
         'form': form
     })
