49c7fb15cf922062e84678b341ea656ec496d074
crisgit24 <cristian.camilo.franco@correounivalle.edu.co>
2025-05-01 15:22:43 -0500

    Arreglo de la rama

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,4 +1,3 @@
 from .cliente import Cliente
-from .egreso import Egreso
 from .reparacion import Reparacion
 from .empleado import Empleado
--- core/services/__init__.py
+++ core/services/__init__.py
@@ -1,5 +1,4 @@
 from .cliente_service import get_all_clientes
-from .egreso_service import crear_egreso, get_all_egreso
 from .reparacion_service import get_all_reparaciones, crear_reparacion
 from .empleado_service import crear_empleado, get_all_empleados
 from .logout_service import cerrar_sesion
