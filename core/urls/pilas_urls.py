0a90661720405f0f07cce92516125b4c0ce2a444
crisgit24 <cristian.camilo.franco@correounivalle.edu.co>
2025-05-10 18:00:15 -0500

    HU de editar pila y refinacion de buscar cliente
    * creacion de HU de egreso terminada
    
    * Merge Migrate
    
    * mejora de pruebas de views de egreso
    
    * Otra prueba de test en egreso
    
    * Test de egreso mejorados
    
    * Tests arreglados
    
    * Merge migrations
    
    * Arreglo de la rama
    
    * Remover BD y coverage
    
    * commit delete_egreso para quedar al dia
    
    * Creacion de HU de buscar cliente
    
    * Delete core/templates/egreso/egreso_list.html
    
    * borrar egreso list
    
    * correcion de mantenibilidad
    
    * editar pila
    
    * mejora de busqueda de cliente
    
    * ultimas mejoras de buscar y pila
    
    ---------
    
    Co-authored-by: David Santiago Cordoba Perdomo <cordoba.david@correounivalle.edu.co>

--- core/urls/pilas_urls.py
+++ core/urls/pilas_urls.py
@@ -4,4 +4,5 @@ from core.views.Pilas.pilas_view import pilas_list_view, pila_create_view
 urlpatterns = [
     path('productos/pilas/', pilas_list_view, name='pilas_list'),
     path('productos/agregar/', pila_create_view, name='pilas_form'),
+    path('productos/editar/<int:id>', pila_create_view, name='pila_editar'),
 ]
