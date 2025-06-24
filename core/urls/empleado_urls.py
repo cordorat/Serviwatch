c2a1d494cf03169da35b6a90a673683e36be3d2c
Anthony Alzate <anthony.alzate@correounivalle.edu.co>
2025-06-23 22:50:57 -0500

    Reporte de empleados

--- core/urls/empleado_urls.py
+++ core/urls/empleado_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from core.views.Empleado.empleado_view import empleado_create_view, empleado_list_view
+from core.views.Empleado.empleado_view import empleado_create_view, empleado_list_view, reporte_empleados_pdf
 
 urlpatterns = [
     path('empleados/', empleado_list_view, name='empleado_list'),
+    path('empleados/reporte', reporte_empleados_pdf, name='reporte_empleados_pdf'),
     path('empleados/agregar/', empleado_create_view, name='empleado_create'),
     path('empleados/editar/<int:id>/', empleado_create_view, name='empleado_update'),
 ]
--- core/views/Empleado/empleado_view.py
+++ core/views/Empleado/empleado_view.py
@@ -5,9 +5,10 @@ from core.services.empleado_service import _initialize_empleado, _handle_form_su
 from django.views.decorators.http import require_http_methods
 from django.db.models import Q
 from core.models.empleado import Empleado
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
+
 
 @require_http_methods(["GET"])
 def empleado_list_view(request,):
@@ -112,3 +113,41 @@ def empleado_create_view(request, id=None):
         'empleado': empleado,
         'success': success
     })
+
+
+
+
+
+@require_http_methods(["GET"])
+def reporte_empleados_pdf(request):
+    """
+    Vista para generar un reporte PDF de empleados con los mismos filtros
+    que se están usando en la vista de lista.
+    """
+    filtro_estado = request.GET.get('estado', '')
+    search_query = request.GET.get('search', '')
+
+    # Usar la misma lógica de filtrado que en empleado_list_view
+    empleados_qs = Empleado.objects.all()
+
+    if filtro_estado and filtro_estado != 'todos':
+        empleados_qs = empleados_qs.filter(estado=filtro_estado)
+
+    if search_query:
+        empleados_qs = empleados_qs.filter(
+            Q(cedula__icontains=search_query) |
+            Q(nombre__icontains=search_query) |
+            Q(apellidos__icontains=search_query)
+        )
+
+    empleados_qs = empleados_qs.order_by('estado')
+    
+    # Generar el PDF
+    from core.services.empleado_service import generar_pdf_empleados
+    pdf = generar_pdf_empleados(empleados_qs, filtro_estado, request)
+    
+    # Devolver el PDF como respuesta HTTP
+    estado_texto = filtro_estado if filtro_estado and filtro_estado != 'todos' else 'todos'
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = f'filename="reporte_empleados_{estado_texto}.pdf"'
+    return response
