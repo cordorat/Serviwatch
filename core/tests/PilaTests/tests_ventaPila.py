67979971815e55ec7db15b7f2d4cae79718a175b
David Santiago Cordoba Perdomo <cordoba.david@correounivalle.edu.co>
2025-06-29 22:00:52 -0500

    Correcciones en links, estilos y tests

--- core/tests/PilaTests/tests_ventaPila.py
+++ core/tests/PilaTests/tests_ventaPila.py
@@ -1,14 +1,10 @@
 from django.test import TestCase, Client, RequestFactory
 from django.urls import reverse
 from django.contrib.auth.models import User
-from django.contrib.messages.storage.fallback import FallbackStorage
 from core.models.pilas import Pilas
 from core.models.VentaPila import VentaPila
 from core.services.pilas_service import update_pila_stock_venta
-from core.views.Pilas.ventaPila_view import ventaPilas_list_view, ventaPila_view
-from datetime import datetime
 from django.contrib.messages import get_messages
-import json
 
 class TestPilasService(TestCase):
     """Pruebas para el servicio de pilas"""
